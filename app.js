28e80a32d69ce6df4fcf0df676044e52f03c3d54
Caio Nunes De Oliveira <caionunez@hotmail.com>
2022-10-22 19:24:42 -0300

    atualizado

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const produto= require("./src/routes/produtoRoute");
-const home = require ("./src/routes/homeRoute")
+const express = require('express')
+const app = express()
+const port = 3001
+const home = require('./src/routes/homeRoute')
+const produto = require('./src/routes/produtoRoute')
 
-app.set ("view engine", "ejs");
-app.set ("views", __dirname + "/src/views");
+app.set('view engine','ejs')
+app.set('views',__dirname + '/src/views')
 
-app.use("/", home);
-app.use("/produto", produto)
+app.use('/', home)
+app.use('/produto',produto)
 
-//app.listen (port, function(){ console.log (`Servidor rodando em http://localhost:${port}`)})
-app.listen (port,()=>{
-    console.log (`Servidor rodando em http://localhost:${port}`)
-})
+app.listen(port,()=>{console.log('servidor rodando na porta '+ port)})
+
+// app.listen (port,()=>{console.log (`Servidor rodando em http://localhost:${port}`)})
